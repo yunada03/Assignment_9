2c890c455244abf5575e6c33eda4d0a80ab716fe
yunada03 <oilhair365@naver.com>
2025-05-07 18:41:18 +0900

    Finalize main.py with all functions

--- main.py
+++ main.py
@@ -1,5 +1,33 @@
 from typing import List
 
+def path_to_file_list(path: str) -> List[str]:
+    """Reads a file and returns a list of lines in the file"""
+    with open(path, 'r', encoding='utf-8') as f:
+        lines = f.read().split('\n')
+    return lines
+
+def train_file_list_to_json(english_file_list: List[str], german_file_list: List[str]) -> List[str]:
+    """Converts two lists of file paths into a list of json strings"""
+    def process_file(file):
+        if '\\' in file:
+            file = file.replace('\\', '\\\\')
+        if '/' or '"' in file:
+            file = file.replace('/', '\\/')
+            file = file.replace('"', '\\"')
+        return file
+
+    template_start = '{\"English\":\"'
+    template_mid = '\",\"German\":\"'
+    template_end = '\"}'
+
+    processed_file_list = []
+    for english_file, german_file in zip(english_file_list, german_file_list):
+        english_file = process_file(english_file)
+        german_file = process_file(german_file)
+
+        processed_file_list.append(template_start + english_file + template_mid + german_file + template_end)
+    return processed_file_list
+
 def write_file_list(file_list: List[str], path: str) -> None:
     """Writes a list of strings to a file, each string on a new line"""
     with open(path, 'w', encoding='utf-8') as f:
